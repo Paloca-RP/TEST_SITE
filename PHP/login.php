0bcb1851279ecdac778c9d89be19de946982b96f
Paloca-RP <ricardopalhoca@hotmail.com>
2020-12-26 18:10:23 +0000

    e

---
+++ PHP/login.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="backend/processaLogin.php" method="POST">
+        <p>Username: <input type="text" name="username" /></p>
+        <p>Password: <input type="password" name="password" /></p>
+        <p><input type="submit" name="submit" value="Login" /></p>
+    </form>
+</body>
+</html>
