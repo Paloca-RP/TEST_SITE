809e72df569320b80e62b433ff0224fc4bfa18a2
Gualdino99 <74073709+Gualdino99@users.noreply.github.com>
2021-01-14 18:38:49 +0000

    corpo

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ function submitComment (){
     var nome = document.getElementbyID("Name1").value
     var comment = document.getElementById("Comment1").value
     
-    if(nome == "" || comment == ""){
+    
         alert("campos vazios");
-    }
+    
 }
