6372dcbd73e3bf55f1f9bc9021a2193c18b556a1
Ricardo Palhoça <73611099+Paloca-RP@users.noreply.github.com>
2021-01-16 16:05:07 +0000

    a

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,67 @@
-function submitComment (){
-    var nome = document.getElementbyID("Name1").value
-    var comment = document.getElementById("Comment1").value
-    
-    
-        alert("campos vazios");
-    
+/*#################_Trocar_Imagem_############################*/
+/*( onMouseOver="mouseEmCimaN(this)" onMouseOut="mouseForaN(this)" )  
+Para ao passar mudar a imagem o "N" é o numero da função */
+//Bioshock
+function mouseEmCima(imagem) {
+    imagem.src="../../images/Trocar_IMG/gow.jpg";
 }
+ function mouseFora(imagem) {  
+    imagem.src="../../images/imagens_principais/BioShock.jpg"; 
+}
+//
+function mouseEmCima1(imagem) {
+    imagem.src="../../images/Trocar_IMG/";
+}
+ function mouseFora1(imagem) {  
+    imagem.src="../../images/imagens_principais/"; 
+}
+//
+function mouseEmCima2(imagem) {
+    imagem.src="../../images/Trocar_IMG/";
+}
+ function mouseFora2(imagem) {  
+    imagem.src="../../images/imagens_principais/"; 
+}
+//
+function mouseEmCima3(imagem) {
+    imagem.src="../../images/Trocar_IMG/";
+}
+ function mouseFora3(imagem) {  
+    imagem.src="../../images/imagens_principais/"; 
+}
+//
+function mouseEmCima4(imagem) {
+    imagem.src="../../images/Trocar_IMG/";
+}
+ function mouseFora4(imagem) {  
+    imagem.src="../../images/imagens_principais/"; 
+}
+//
+function mouseEmCima5(imagem) {
+    imagem.src="../../images/Trocar_IMG/";
+}
+ function mouseFora5(imagem) {  
+    imagem.src="../../images/imagens_principais/"; 
+}
+//
+function mouseEmCima6(imagem) {
+    imagem.src="../../images/Trocar_IMG/";
+}
+ function mouseFora6(imagem) {  
+    imagem.src="../../images/imagens_principais/"; 
+}
+//
+function mouseEmCima7(imagem) {
+    imagem.src="../../images/Trocar_IMG/";
+}
+ function mouseFora7(imagem) {  
+    imagem.src="../../images/imagens_principais/"; 
+}
+//
+function mouseEmCima8(imagem) {
+    imagem.src="../../images/Trocar_IMG/";
+}
+ function mouseFora8(imagem) {  
+    imagem.src="../../images/imagens_principais/"; 
+}
+/*#################_Trocar_Imagem_############################*/
