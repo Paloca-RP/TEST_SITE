de438560494e662507064235113296bf38ef29ed
Ricardo Palhoça <73611099+Paloca-RP@users.noreply.github.com>
2021-02-15 15:28:10 +0000

    b

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,1 @@
-function calIMC(){
-    var altura = document.getElementById("altura").value;
-    var peso = document.getElementById("peso").value;
-    let imc = (peso /( altura*altura));
-    document.getElementById("resultado").innerHTML = imc;
-}
+s
